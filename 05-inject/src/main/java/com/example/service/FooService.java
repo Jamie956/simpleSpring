1e303451a6cda5893f250a42906461eb114888a3
jamie <jamie@justek.us>
2018-05-07 16:57:48 +0800

    add inject

---
+++ 05-inject/src/main/java/com/example/service/FooService.java
@@ -0,0 +1,5 @@
+package com.example.service;
+
+public interface FooService {
+	public int doSomething();
+}
---
+++ 05-inject/src/main/java/com/example/service/FooServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FooServiceImpl implements FooService {
+	public int doSomething() {
+		// Do some stuff here
+		return 0;
+	}
+}
