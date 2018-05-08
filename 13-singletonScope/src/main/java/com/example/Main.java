3ac2c0c9b80a6c5ccaad3b6cba1cebbe3d7a102e
jamie956 <752492509@qq.com>
2018-05-08 22:40:23 +0800

    lazy singleton

--- 13-singletonScope/src/main/java/com/example/Main.java
+++ 13-singletonScope/src/main/java/com/example/Main.java
@@ -7,10 +7,12 @@ public class Main {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SingletonConfiguration.class);
 		MyBean singletonBean1 = context.getBean("singletonBean", MyBean.class);
 		singletonBean1.setProperty("changed property");
-		
+
 		MyBean singletonBean2 = context.getBean("singletonBean", MyBean.class);
-		
-		System.out.println(singletonBean1.getProperty());
-		System.out.println(singletonBean2.getProperty());
+
+		// System.out.println(singletonBean1.getProperty());
+		// System.out.println(singletonBean2.getProperty());
+		System.out.println("Retrieving lazy singleton bean...");
+		MyBean singletonBean3 = (MyBean) context.getBean("lazySingletonBean");
 	}
 }
--- 13-singletonScope/src/main/java/com/example/SingletonConfiguration.java
+++ 13-singletonScope/src/main/java/com/example/SingletonConfiguration.java
@@ -2,11 +2,18 @@ package com.example;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class SingletonConfiguration {
 	@Bean
 	public MyBean singletonBean() {
 		return new MyBean("singleton");
+	}
+
+	@Bean
+	@Lazy
+	public MyBean lazySingletonBean() {
+		return new MyBean("lazy singleton");
 	}
 }
