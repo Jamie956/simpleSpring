a307501812dfa4d4aaed6eecfddcdeb056ea3f70
Unknown <752492509@qq.com>
2018-10-05 22:06:36 +0800

    up

--- 05-inject/src/main/java/com/example/service/OrderService.java
+++
@@ -1,5 +0,0 @@
-package com.example.service;
-
-public interface OrderService {
-	public void doSomething();
-}
--- 05-inject/src/main/java/com/example/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.example.service;
-
-public interface UserService {
-	public void doSomething();
-}
