090508915c491e176c3d099cdfe9b6e14d70147e
Unknown <752492509@qq.com>
2018-10-06 09:36:18 +0800

    up

---
+++ scope/src/main/java/v4/MyConfig.java
@@ -0,0 +1,14 @@
+package v4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class MyConfig {
+	@Bean
+	@Scope("prototype")
+	public User userBean() {
+		return new User("tom");
+	}
+}
