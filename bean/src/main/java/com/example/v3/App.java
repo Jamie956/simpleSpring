45fa4f02b45cbafdff536d1b759530a20aacd3de
Unknown <752492509@qq.com>
2018-10-06 08:51:39 +0800

    up

--- bean/src/main/java/com/example/v3/App.java
+++ bean/src/main/java/com/example/v3/App.java
@@ -1,13 +1,14 @@
 package com.example.v3;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/example/v3/beans.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/example/v3/beans.xml");
 		User user = (User) context.getBean("userBean");
 		String name = user.getName();
 		System.out.println(name);
+		
+		context.close();
 	}
 }
--- bean/src/main/java/com/example/v4/App.java
+++ bean/src/main/java/com/example/v4/App.java
@@ -1,13 +1,14 @@
 package com.example.v4;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/example/v4/beans.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/example/v4/beans.xml");
 		User user = (User) context.getBean("userBean");
 		String name = user.getName();
 		System.out.println(name);
+		
+		context.close();
 	}
 }
