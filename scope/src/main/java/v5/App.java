8d73f311b82f4d1bc96a35788b1fee0d457ca000
Unknown <752492509@qq.com>
2018-10-06 09:53:26 +0800

    up

---
+++ scope/src/main/java/v5/App.java
@@ -0,0 +1,21 @@
+package v5;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+		Book b1 = (Book) context.getBean("bookBean");
+		b1.setName("HTML");
+		Book b2 = (Book) context.getBean("bookBean");
+
+		System.out.println(b1.getName());
+		System.out.println(b2.getName());
+
+		Book s3 = (Book) context.getBean("bookBeanLazy");
+		System.out.println(s3.getName());
+
+		context.close();
+	}
+}
---
+++ scope/src/main/java/v5/Book.java
@@ -0,0 +1,19 @@
+package v5;
+
+public class Book {
+	private String name;
+
+	public Book(String name) {
+		this.name = name;
+		System.out.println("Book cons, "+name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
