dba174971b7b2ed51a375db12f6ab54b0149b740
Unknown <752492509@qq.com>
2018-10-06 15:24:25 +0800

    up

--- scope/src/main/java/v1/App.java
+++ scope/src/main/java/v1/App.java
@@ -1,16 +1,17 @@
 package v1;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("v1/beans.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("v1/beans.xml");
 		WindowGenerator windowGenerator = (WindowGenerator) context.getBean("windowGenerator");
 		
 		Window window = windowGenerator.createWindow();
 		Window window2 = windowGenerator.createWindow();
 		
 		System.out.println(window == window2);
+		
+		context.close();
 	}
 }
